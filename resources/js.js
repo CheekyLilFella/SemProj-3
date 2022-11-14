bd8a034f075888fb8c1d70bc85796bae639dd0df
Marinus Bach Oestrich <maroes21@student.sdu.dk>
2022-11-14 15:38:33 +0100

    mappe sortering

---
+++ resources/js.js
@@ -0,0 +1,47 @@
+// When the user scrolls the page, execute myFunction
+ window.onscroll = function() {myFunction()};
+
+function myFunction() {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    document.getElementById("myBar").style.height = scrolled + "%";
+    depth.innerHTML = "depth: " + Math.round(scrolled/100*10911);
+};
+
+function titelTextFunction() {
+    var gennemsigtighed = $(".top").css("opacity");
+    if(gennemsigtighed < 0.5) {
+    $(".titelTekst").css('color', 'white');
+    } else {
+        $(".titelTekst").css('color', 'black');
+    };
+};
+
+$(document).ready(function() {
+    setInterval("titelTextFunction()", 500);
+});
+
+
+// Flashlight Mouse Pointer
+function update(e){
+    var x = e.clientX || e.touches[0].clientX
+    var y = e.clientY || e.touches[0].clientY
+  
+    document.documentElement.style.setProperty('--cursorX', x + 'px')
+    document.documentElement.style.setProperty('--cursorY', y + 'px')
+}
+  
+document.addEventListener('mousemove',update)
+document.addEventListener('touchmove',update)
+  
+// When the user scrolls the page, execute myFunction 
+window.onscroll = function() {myFunction()};
+        
+function myFunction() {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    document.getElementById("myBar").style.height = scrolled + "%";
+    depth.innerHTML = "depth: " + Math.round(scrolled/100*10911);
+}
