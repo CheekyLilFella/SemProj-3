7f4033287173b51d97be258300ff72a2d0e02c15
Marinus Bach Oestrich <maoes21@student.sdu.dk>
2022-11-17 14:12:15 +0100

    Merge, cursor og mapper

--- resources/javascript/js.js
+++ resources/javascript/js.js
@@ -35,14 +35,3 @@ function update(e){
   
 document.addEventListener('mousemove',update)
 document.addEventListener('touchmove',update)
-  
-// When the user scrolls the page, execute myFunction 
-window.onscroll = function() {myFunction()};
-        
-function myFunction() {
-    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-    var scrolled = (winScroll / height) * 100;
-    document.getElementById("myBar").style.height = scrolled + "%";
-    depth.innerHTML = "depth: " + Math.round(scrolled/100*10911);
-}
